a27330a812f7ee7e6b60d337d3e3f4f5d6279867
Faiz Ahmed <4faizahmed@gmail.com>
2022-08-30 09:05:20 +0300

    Project configuration: scss, fonts, bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
+import {useEffect} from "react";
+import Head from "next/head";
+import '../styles/globals.scss';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.bootstrap = require('bootstrap');
+  }, [])
   return <Component {...pageProps} />
 }
 
